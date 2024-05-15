6830e13886696e13b1c74f97b27d83f96441351f
Z4dEV <z4kreyyt@gmail.com>
2024-05-15 09:22:37 +0100

    Removal of mongodb variable and console.log

--- back-end/server.js
+++ back-end/server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
 const contractrouter = require('./routers/contractRouter.js')
 
 const app = express();
 
-const mongoURI = process.env.MONGO_DB_URI;
-console.log(mongoURI)
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 app.use(express.json());
-
 app.use(contractrouter);
 
 const port = process.env.PORT || 5000;
